1b5d028f8cb85179b9d711d665193ef9104302a2
PinnaniMokshagna <pinnanimokshagna@gmail.com>
2025-07-18 14:05:15 +0530

    Add files via upload

---
+++ Test submisson/complete code/22VE1A67A3-main/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UrlShortener from '.UrlShortener';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6"> URL Shortener</h1>
+      <UrlShortener />
+    </div>
+  );
+}
+
+export default App;
